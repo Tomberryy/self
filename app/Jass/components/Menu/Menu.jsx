16982e49bcfba78b818c18c0d94168cd26d5a08f
Antoine <antoine.monnin@gmail.com>
2023-07-26 10:20:33 +0200

    feat: BG-color animations (WIP)

--- app/Jass/components/Menu/Menu.jsx
+++ app/Jass/components/Menu/Menu.jsx
@@ -4,18 +4,43 @@ import { Col, Row } from 'reactstrap';
 const Menu = ({onSetParties, onSetTheme, theme}) => {
     const [parties, setParties] = useState([]);
     const themes = [
-        'dark',
-        'white',
-        'purple',
-        'darkblue',
-        'lightblue'
+        {
+            name: 'dark',
+            textColor: 'black',
+            border: 'white'
+        },
+        {
+            name: 'white',
+            textColor: 'white',
+            border: 'black'
+        },
+        {
+            name: 'orange',
+            textColor: 'peru',
+            border: 'black'
+        },
+        {
+            name: 'darkblue',
+            textColor: 'darkblue',
+            border: 'white'
+        },
+        {
+            name: 'lightblue',
+            textColor: 'lightblue',
+            border: 'black'
+        },
     ]
     return (
         <div className='Menu h-100'>
             <Row className='h-100'>
                 <Col sm="12" className="theme-choice d-flex justify-content-center align-items-center">
                     {themes.map(t => 
-                        <i key={t} onClick={() => onSetTheme(t)} className={`bi bi-circle${t === theme ? "" : "-fill"} ${t}`}></i>
+                        <i
+                            key={t.name}
+                            onClick={() => onSetTheme(t.name)}
+                            className={`bi bi-circle${t.name === theme ? "" : "-fill"}`}
+                            style={{color : t.name === theme ? t.border : t.textColor}}
+                        />
                     )}
                 </Col>
                 <Col sm="12" className='border-top name-choice'></Col>
