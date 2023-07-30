98f832aa9cef588727ba3f4bba0cf02338fd24fb
Antoine Monnin <antoine.monnin@gmail.com>
2023-07-30 21:19:23 +0200

    feat: Add store (Redux)

--- app/Jass/components/Pannels/Overview.jsx
+++ app/Jass/components/Pannels/Overview.jsx
@@ -1,9 +1,17 @@
+import { useState } from "react";
 import Canvas from "./Canvas";
 
 const Overview = () => {
+    function randomIntFromInterval(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+    const [score, setScore] = useState(randomIntFromInterval(1, 1000));
+
     return (
         <div className="Overview h-100">
-            <Canvas />
+            <Canvas score={score} />
+            <button onClick={() => setScore(randomIntFromInterval(1, 1000))}>TOAST</button>
+            <label>Score : {score}</label>
         </div>
     );
 };
--- app/main.jsx
+++ app/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Jass from './Jass/Jass.jsx';
 import './index.css'
+import store from './store'
+import { Provider } from 'react-redux'
 import {
     createBrowserRouter,
     RouterProvider,
@@ -20,7 +22,7 @@ const router = createBrowserRouter([
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
+    <Provider store={store}>
         <RouterProvider router={router} />
-    </React.StrictMode>,
+    </Provider>
 )
