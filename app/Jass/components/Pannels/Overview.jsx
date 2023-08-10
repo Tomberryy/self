e82ce62fa8a5257917340f37184adee8222afa2b
Antoine Monnin <antoine.monnin@gmail.com>
2023-08-10 11:22:37 +0200

    Add title to parties (Name and score)

--- app/Jass/components/Pannels/Overview.jsx
+++ app/Jass/components/Pannels/Overview.jsx
@@ -1,20 +1,24 @@
-import { useState } from "react";
-import Canvas from "./Canvas";
+import Canvas from "./PannelsComponents/Canvas";
 import { useSelector } from "react-redux";
 
 const Overview = () => {
-    const { parties } = useSelector(state => state.jass);
+    const { parties } = useSelector((state) => state.jass);
 
     if (!parties)
         return (
-        <div className="spinner-border" role="status">
-            <span className="visually-hidden">Loading...</span>
-        </div>
-        )
+            <div className="spinner-border" role="status">
+                <span className="visually-hidden">Loading...</span>
+            </div>
+        );
+
+    console.log(parties, parties.partyOne.score);
     return (
         <div className="Overview h-100">
-            <Canvas data={parties.partyOne.score} />
-            <Canvas data={parties.partyTwo.score} />
+
+            <h2>{parties.partyOne.name} - {parties.partyOne.score}</h2>
+            <Canvas score={parties.partyOne.score} />
+            <h2>{parties.partyTwo.name} - {parties.partyTwo.score}</h2>
+            <Canvas score={parties.partyTwo.score} />
         </div>
     );
 };
